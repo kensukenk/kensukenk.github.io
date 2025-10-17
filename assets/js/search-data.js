97365f3b934263220b627dc0a87d6ccd17b71557
kensukenk <72209286+kensukenk@users.noreply.github.com>
2025-10-17 03:02:22 +0000

    Deploying to gh-pages from @ kensukenk/kensukenk.github.io@0ede59990c599765828f822b47b3527c7fcf14d2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -64,6 +64,9 @@ ninja.data = [{
           section: "News",},{id: "news-our-new-paper-strengthens-latent-safety-filters-by-allowing-them-to-account-for-out-of-distribution-failures-by-treating-regions-where-a-world-model-has-high-uncertainty-as-a-safety-violation-this-work-was-lead-by-junwon-seo-and-the-project-website-is-here",
           title: 'Our new paper strengthens latent safety filters by allowing them to account for...',
           description: "",
+          section: "News",},{id: "news-two-new-papers-extending-latent-safety-filters-in-anysafe-we-explore-how-to-parameterized-latent-safety-filters-in-order-to-flexibly-specify-constraints-at-deployment-time-in-our-second-work-we-study-temperature-based-constraints-to-investigate-how-partial-observability-can-degrade-the-ability-of-latent-safety-filters-to-prevent-safety-violations-we-quantify-partial-observability-in-the-latent-space-of-self-supervised-world-models-using-mutual-information-and-provide-a-multimodal-training-strategy-for-effective-filtering-even-under-partial-observability",
+          title: 'Two new papers extending latent safety filters! In Anysafe we explore how to...',
+          description: "",
           section: "News",},{
         id: 'social-email',
         title: 'email',
